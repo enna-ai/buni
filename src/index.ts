a870348669c34d796fda6cb9442d903771f5344a
enna-ai <annelee3322@gmail.com>
2024-03-12 10:51:52 -0600

    ♻️  refactor code

--- src/index.ts
+++ src/index.ts
@@ -3,72 +3,51 @@ import chalk from "chalk";
 import { deleteAsync } from "del";
 import { spawnSync } from "child_process";
 
-const searchBun = () => {
-    const bunLockPath = "bun.lockb";
-    if (fs.existsSync(bunLockPath)) {
-        console.log(`\n${chalk.red("✘")} ${chalk.gray("bun.lockb found.")} This project is already using bun.`);
+const checkForBunFile = (target: string) => {
+    if (fs.existsSync(target)) {
+        console.log(`${chalk.red("✘")} ${chalk.gray("bun.lockb found!")} This project is already using bun.`);
         process.exit(1);
     }
-}
-
-const convertToBun = async () => {
-    try {
-        const yarnLockPath = "yarn.lock";
-        if (fs.existsSync(yarnLockPath)) {
-            console.log(`${chalk.green("✔")} yarn.lock found!`);
-            console.log(`${chalk.gray("Removing yarn.lock...")}`);
-
-            await deleteAsync(yarnLockPath);
-            spawnSync("bun", ["i"], {
-                stdio: "inherit",
-                shell: true,
-            });
-        }
-
-        const nodeModulesPath = "node_modules";
-        if (fs.existsSync(nodeModulesPath)) {
-            console.log(`${chalk.green("✔")} node_modules found!`);
-            console.log(`${chalk.gray("Removing node_modules...")}`);
-
-            await deleteAsync(nodeModulesPath);
-            spawnSync("bun", ["i"], {
-                stdio: "inherit",
-                shell: true,
-            });
-        }
-
-        const packageLockPath = "package-lock.json";
-        if (fs.existsSync(packageLockPath)) {
-            console.log(`${chalk.green("✔")} package-lock.json found!`);
-            console.log(`${chalk.gray("Removing package-lock.json")}`);
-
-            await deleteAsync(packageLockPath);
-            spawnSync("bun", ["i"], {
-                stdio: "inherit",
-                shell: true,
-            });
-        }
-
-        const pnpmLockPath = "pnpm-lock.yaml";
-        if (fs.existsSync(pnpmLockPath)) {
-            console.log(`${chalk.green("✔")} pnpm-lock.yaml found!`);
-            console.log(`${chalk.gray("Removing pnpm-lock.yaml")}`);
-
-            await deleteAsync(pnpmLockPath);
-            spawnSync("bun", ["i"], {
-                stdio: "inherit",
-                shell: true,
-            });
-        }
-    } catch (error: any) {
-        console.error(`An error occured: ${error.message}`);
-        process.exit(1);
-    }
-}
-
-const bunify = async () => {
-    searchBun();
-    await convertToBun();
 };
 
-bunify();
+const deleteNodeModulesFile = async () => {
+    if (fs.existsSync("node_modules")) {
+        console.log(`${chalk.green("✔")} node_modules found!`);
+        console.log(`${chalk.gray("Removing node_modules...")}`);
+        await deleteAsync("node_modules");
+    }
+};
+
+const installBun = async () => {
+    try {
+        spawnSync("bun", ["i"], {
+            stdio: "inherit",
+            shell: true,
+        });
+    } catch (error) {
+        console.error(`${chalk.red("✘")} Error installing bun: ${error}`);
+        process.exit(1);
+    }
+};
+
+const convertToBun = async (lockFiles: string[], target: string) => {
+    checkForBunFile(target);
+
+    for (const lockFile of lockFiles) {
+        try {
+            if (!fs.existsSync(lockFile)) continue;
+
+            console.log(`${chalk.green("✔")} ${lockFile} found!`);
+            console.log(`${chalk.gray(`Removing ${lockFile}...`)}`);
+            await deleteAsync(lockFile);
+        } catch (error) {
+            console.error(`${chalk.red("✘")} Error installing bun: ${error}`);
+            process.exit(1);
+        }
+    }
+    
+    await deleteNodeModulesFile();
+    await installBun();
+};
+
+convertToBun(["yarn.lock", "package-lock.json", "pnpm-lock.yaml"], "bun.lockb");
